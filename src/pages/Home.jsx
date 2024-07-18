87db10711eda05f148dded5c32a9ae79f434a7fe
Edna-codes <guantaiedna2@gmail.com>
2024-07-18 13:32:40 +0300

    Add background image to the landing page and style it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
-
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
-
-    <div>
+      <div className="body">
         <h1>Home</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum reiciendis quam, architecto culpa nesciunt ullam nobis odit accusantium voluptatum deleniti explicabo autem a facere! Fugit nisi nam vitae eaque ut neque reiciendis reprehenderit. Deleniti nemo eum sapiente laborum, aut magni a id, ea saepe vitae unde minima facilis officia dolorem totam fugiat culpa fuga. Quasi deleniti dolorum quae at quis sequi dolor neque assumenda enim repellendus, dolores animi optio laboriosam deserunt iure, itaque dolore, porro aperiam distinctio perferendis accusantium quisquam culpa reiciendis error. Cupiditate esse quos sit totam, consequatur saepe minima soluta earum debitis autem rerum illum quisquam aliquam. Magni.</p>
-    </div>
+      </div>
     </>
-  )
+  );
 }
